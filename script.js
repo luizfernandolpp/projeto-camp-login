9d1ac44786709edc96e7931b569d924b8a121117
Luiz Fernando <111098745+luizfernandolpp@users.noreply.github.com>
2023-03-06 03:03:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function mouseOverMail(){
+    let logo = document.querySelector('span#mail')
+    logo.style.color = 'var(--primary-color)'
+}
+
+function mouseOutMail(){
+    let logo = document.querySelector('span#mail')
+    logo.style.color = 'var(--text-base)'
+}
+
+function mouseOverLock(){
+    let logo = document.querySelector('span#lock')
+    logo.style.color = 'var(--primary-color)'
+}
+
+function mouseOutLock(){
+    let logo = document.querySelector('span#lock')
+    logo.style.color = 'var(--text-base)'
+}
+
+function clickMail(){
+    let logo = document.querySelector('span#mail')
+    logo.style.color = 'var(--text-base)'
+}
+
+function clickLock(){
+    let logo = document.querySelector('span#lock')
+    logo.style.color = 'var(--text-base)'
+}
