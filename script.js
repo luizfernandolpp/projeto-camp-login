2c5d0836d31240a19690c676be43b5d0d4eeb48d
Luiz Fernando <111098745+luizfernandolpp@users.noreply.github.com>
2023-03-06 03:45:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ function mouseOutMail(){
 
 function mouseOverLock(){
     let logo = document.querySelector('span#lock')
+    let vis = document.querySelector('span#vis')
+    vis.style.color = 'var(--primary-color)'
     logo.style.color = 'var(--primary-color)'
 }
 
 function mouseOutLock(){
     let logo = document.querySelector('span#lock')
+    let vis = document.querySelector('span#vis')
+    vis.style.color = 'var(--text-base)'
     logo.style.color = 'var(--text-base)'
 }
 
@@ -25,5 +29,22 @@ function clickMail(){
 
 function clickLock(){
     let logo = document.querySelector('span#lock')
+    let vis = document.querySelector('span#vis')
+    vis.style.color = 'var(--text-base)'
     logo.style.color = 'var(--text-base)'
+}
+
+
+function ShowHide(){             // Função que mostra/esconde a senha conforme o usuário clica no ícone do "olhinho"
+    let vis = document.querySelector('span#vis')
+    let passContent = document.querySelector('input#ipass')
+    if (vis.innerHTML == 'visibility'){
+        vis.innerHTML = 'visibility_off'
+        passContent.removeAttribute('type', 'text')
+        passContent.setAttribute('type', 'password')
+    } else if (vis.innerHTML == 'visibility_off'){
+        vis.innerHTML ='visibility'
+        passContent.removeAttribute('type', 'password')
+        passContent.setAttribute('type', 'text')
+    }
 }
